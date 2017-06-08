0771a9fa6c38573ba9725908cafeb453a41a1a63
Waleed Usman <waleed.usman@covata.com>
2017-06-08 23:23:35 +1000

    Add comment about configuring log format

--- src/main/java/com/example/TrafficLightSimulator.java
+++ src/main/java/com/example/TrafficLightSimulator.java
@@ -2,6 +2,7 @@ package com.example;
 
 public class TrafficLightSimulator {
     public static void main(String[] args) {
+        // set the log output to be in a single line
         System.setProperty(
                 "java.util.logging.SimpleFormatter.format",
                 "%1$tY-%1$tm-%1$td %1$tH:%1$tM:%1$tS %4$-6s %5$s%6$s%n");
